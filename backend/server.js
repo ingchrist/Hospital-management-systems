ff59826bf44b1f365a86c9c6c2068d3c3ace63e8
root <root@ip-172-31-31-168.ec2.internal>
2024-05-09 22:27:37 +0000

    files added

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,13 @@ import mongoose from "mongoose";
 import { postsRoutes } from "./routes/postsRoutes.js";
 import { usersRoutes } from "./routes/usersRoutes.js";
 
+import path from 'path';
+import { fileURLTOPath } from 'URL';
+
+//resolving dirnames for ES module
+const __filename = fileURLTOPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 // Initializing Express app
 const app = express();
 
@@ -12,6 +19,14 @@ app.use(express.json());
 // Adding the API end-points and the route handlers
 app.use("/api/posts", postsRoutes);
 app.use("/api/users", usersRoutes);
+
+//use the client app
+app.use(express.static(path.join(__dirname, '/client/dist')))
+
+//render client for any path
+app.get('*', (req, res) => 
+  res.sendFile(path.join(__dirname, '/client/dist/index.html'))
+);
 
 // Connecting to MongoDB using Mongoose
 mongoose
@@ -23,3 +38,4 @@ mongoose
     app.listen(4000, "localhost", () => console.log("Listening to port 4000"));
   })
   .catch((err) => console.log(err));
+
