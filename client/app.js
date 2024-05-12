a1f726d993da927a7e39f0997aa928312e9f4291
ingchrist <cdjinguet@gmail.com>
2024-05-12 12:24:18 +0000

    files added

--- client/app.js
+++ client/app.js
@@ -5,7 +5,7 @@ function App() {
 
   // Fetching message from backend on mount
   useEffect(() => {
-    fetch("http://localhost:4000")
+    fetch("https://hospital-management-systems-3.onrender.com")
       .then((res) => res.json())
       .then((data) => setMessage(data.message));
   }, []);
